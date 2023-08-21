bf363b6dcda97ec6a4f19d40270c2c81e8805359
Kyle Washington <kmw1993@msn.com>
2023-08-21 15:53:24 -0400

    Added Dbset which will have the contents of my table filled     once migrated and updated

--- MoviesAPI/MoviesAPI/Data/ApplicationDbContext.cs
+++ MoviesAPI/MoviesAPI/Data/ApplicationDbContext.cs
@@ -1,13 +1,24 @@
 ﻿using Microsoft.EntityFrameworkCore;
-
+using MoviesAPI.Models;
 namespace MoviesAPI.Data
 {
-    public class ApplicationDbContext :DbContext
+    public class ApplicationDbContext : DbContext
     {
-        public ApplicationDbContext(DbContextOptions options) : base(options) 
+        public DbSet<Movie> Movies { get; set; }
+        public ApplicationDbContext(DbContextOptions options) : base(options)
         {
-
         }
+        protected override void OnModelCreating(ModelBuilder ModelBuilder)
+        {
+            base.OnModelCreating(ModelBuilder);
 
+            ModelBuilder.Entity<Movie>().
+                HasData(
+                new Movie { Id = 1, Title = "The Matrix", RunningTime = 127, Genre = "Action" },
+                new Movie { Id = 2, Title = "Jurrasic Park", RunningTime = 127, Genre = "Action" },
+                new Movie { Id = 3, Title = "Shrek", RunningTime = 90, Genre = "Animation" },
+                new Movie { Id = 4, Title = "Hereditary", RunningTime = 127, Genre = "Horror" }
+                );
+        }
     }
 }
