0d0f6091067351b8f5098cde17904f48c64a3b45
Kyle Washington <kmw1993@msn.com>
2023-08-21 16:23:19 -0400

    Added my controller with logic for each endpoint

---
+++ MoviesAPI/MoviesAPI/Controllers/MoviesController.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MoviesAPI.Models;
+using MoviesAPI.Data;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MoviesAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public MoviesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+
+        // GET: api/<Movies>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var movies = _context.Movies.ToList();
+            return Ok(movies);
+        }
+
+        // GET api/<Movies>/5
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var movie = _context.Movies.Find(id);
+            if (movie == null)
+                return NotFound();
+
+            return Ok(movie);
+        }
+
+        // POST api/<Movies>
+        [HttpPost]
+        public IActionResult Post([FromBody] Movie movie)
+        {
+            if (movie == null) return BadRequest();
+            _context.Movies.Add(movie);
+            _context.SaveChanges();
+
+            return CreatedAtAction(nameof(Get), new { id = movie.Id }, movie);
+        }
+
+        // PUT api/<Movies>/5
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] Movie updatedMovie)
+        {
+            var existingMovie = _context.Movies.Find(id);
+            if (existingMovie == null)
+                return NotFound();
+
+            existingMovie.Title = updatedMovie.Title;
+            existingMovie.RunningTime = updatedMovie.RunningTime;
+            existingMovie.Genre = updatedMovie.Genre;
+
+            _context.SaveChanges();
+
+            return Ok();
+        }
+
+        // DELETE api/<Movies>/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var movie = _context.Movies.Find(id);
+            if (movie == null) return NotFound();
+
+            _context.Movies.Remove(movie);
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+    }
+}
---
+++ MoviesAPI/MoviesAPI/Migrations/20230821195445_initial.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace MoviesAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Movies",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Title = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    RunningTime = table.Column<int>(type: "int", nullable: false),
+                    Genre = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Movies", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.InsertData(
+                table: "Movies",
+                columns: new[] { "Id", "Genre", "RunningTime", "Title" },
+                values: new object[,]
+                {
+                    { 1, "Action", 127, "The Matrix" },
+                    { 2, "Action", 127, "Jurrasic Park" },
+                    { 3, "Animation", 90, "Shrek" },
+                    { 4, "Horror", 127, "Hereditary" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Movies");
+        }
+    }
+}
